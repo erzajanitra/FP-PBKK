d762318efe455d65b9e31b68c2a20f5f2b7b7036
erzajanitra <75319371+erzajanitra@users.noreply.github.com>
2022-05-18 19:10:27 +0700

    update dashboard
    image carousel and desc about Bromo Adventure

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,11 @@
 @section('title', 'Dashboard Login')
 
 <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 <x-app-layout>
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
@@ -14,9 +19,46 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+           
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="img/bromo.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/bromo2.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/bromo3.jpeg" alt="Third slide">
+                </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Selamat datang di website Bromo Adventure 2022!
+                    <h4>Selamat datang di website Bromo Adventure 2022!</h2>
+                    {{-- <h2> Bromo Adventure </h2> --}}
+                    {{-- <img src="/public/img/Erza.png" alt="Girl in a jacket" style="width:500px;height:600px;"> --}}
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <p>
+                        Bromo Adventure menyediakan akses bagi masyarakat untuk menemukan dan memesan berbagai layanan transportasi, akomodasi, aktivitas selama berwisata di Bromo. 
+                        Sebagai penyedia layanan wisata nomor satu di Bromo, Bromo Adventure memiliki portofolio produk yang lengkap meliputi layanan transportasi serperti sewa mobil 
+                        jeep dan antar jemput ke destinasi wisata. Bromo Adventure menawarkan bantuan untuk pembelian tiket masuk ke kawasan wisata Puncak Bromo, baik secara online dan offline. 
+                    </p>
+                    {{-- <img src="img/bromo.jpg" alt="Girl in a jacket" style="width:500px;"> --}}
                 </div>
             </div>
         </div>
