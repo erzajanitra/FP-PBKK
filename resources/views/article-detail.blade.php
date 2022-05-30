66d09e71a35f6402ae3f16da6b534e057d2ced53
florentinobenedictus <85059763+florentinobenedictus@users.noreply.github.com>
2022-05-30 21:05:14 +0700

    fix justify

--- resources/views/article-detail.blade.php
+++ resources/views/article-detail.blade.php
@@ -8,18 +8,18 @@
     <link rel="stylesheet" href="/css/article.css">
     {{-- Font Poppins --}}
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-    <div class="header" style="text-align: center;">
-        <h1 style="padding-top: 5%; font-family: Poppins;">{{ $article->title }}</h1>
+    <div class="header">
+        <h1 style="padding-top: 5%; font-family: Poppins;text-align: center;">{{ $article->title }}</h1>
 
         <hr style="height:5px;border-width:0;color:#1CE088;background-color:#1CE088">
         
-        <h5>By: <a href="#" class="text-decoration-none" style="color:#01F9C6">{{ $article->user->name }}</a></h6>
+        <h5 style="text-align: center;">By: <a href="#" class="text-decoration-none" style="color:#01F9C6">{{ $article->user->name }}</a></h5>
 
         <article class="mb-5">
             <p align="justify">{!! $article->body !!}</p>
         </article>
 
-        <h5><a href="/article" class="btn">Back to Articles</a></h5>
+        <h5 style="text-align: center;"><a href="/article" class="btn">Back to Articles</a></h5>
         
     </div>
 @endsection
